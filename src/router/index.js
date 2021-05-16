4aeddb74ce44ca0c6ecee5e6e9fc647170418222
Ibra <ibragimgiyazov@gmail.com>
2021-05-17 01:43:45 +0300

    CHange ip

--- src/router/index.js
+++ src/router/index.js
@@ -13,68 +13,69 @@ import SearchCatalog from '../views/SearchCatalog.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/catalog',
-    name: 'Catalog',
-    component: Catalog
-  },
-  {
-    path: '/cstore',
-    name: 'CStore',
-    component: CStore
-  },
-  {
-    path: '/course/:id',
-    name: 'Course',
-    component: Course
-  },
-  {
-    path: '/signin',
-    name: 'Signin',
-    component: Signin
-  },
-  {
-    path: '/author/:id',
-    name: 'Author',
-    component: Author
-  },
-  {
-    path: '/lk',
-    name: 'Lk',
-    component: Lk
-  },
-  {
-    path: '/saved',
-    name: 'Saved',
-    component: Saved
-  },
-  {
-    path: '/searchcatalog',
-    name: 'SearchCatalog',
-    component: SearchCatalog
-  },
-  
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/catalog',
+        name: 'Catalog',
+        component: Catalog
+    },
+    {
+        path: '/cstore',
+        name: 'CStore',
+        component: CStore
+    },
+    {
+        path: '/course/:id',
+        name: 'Course',
+        component: Course
+    },
+    {
+        path: '/signin',
+        name: 'Signin',
+        component: Signin
+    },
+    {
+        path: '/author/:id',
+        name: 'Author',
+        component: Author
+    },
+    {
+        path: '/lk',
+        name: 'Lk',
+        component: Lk
+    },
+    {
+        path: '/saved',
+        name: 'Saved',
+        component: Saved
+    },
+    {
+        path: '/searchcatalog',
+        name: 'SearchCatalog',
+        component: SearchCatalog
+    },
+
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes,
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  }
+    mode: 'history',
+    routes,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
+    }
 })
 
 export default router
