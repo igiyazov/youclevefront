c02be341cec225ba5fb29e0bc900090641757e19
Ibra <ibragimgiyazov@gmail.com>
2021-06-07 13:33:16 +0300

    Set fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -248,8 +248,8 @@ export default {
                 password:"",
                 username:"", 
             },
-            emailAut:"admin@admin.co",
-            passwordAut:"admin",
+            emailAut:"",
+            passwordAut:"",
             secPass:""
 
         }
